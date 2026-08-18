114cf42831f58ec045ed588b403c934a909a9c14
dolodorsey <dolodorsey@gmail.com>
2026-08-18 03:26:26 -0400

    security: minimize BOH tRPC surface to current operational routers

--- expo/backend/trpc/app-router.ts
+++ expo/backend/trpc/app-router.ts
@@ -1,24 +1,14 @@
 import { createTRPCRouter } from './create-context';
-import { exampleRouter } from './routes/example';
 import { communicationsRouter } from './routes/communications';
 import { dashboardRouter } from './routes/dashboard';
 import { entitiesRouter } from './routes/entities';
 import { executionsRouter } from './routes/executions';
-import { activityRouter } from './routes/activity';
-import { cultureRouter } from './routes/culture';
-import { profilesRouter } from './routes/profiles';
-import { locationsRouter } from './routes/locations';
 
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   communications: communicationsRouter,
   dashboard: dashboardRouter,
   entities: entitiesRouter,
   executions: executionsRouter,
-  activity: activityRouter,
-  culture: cultureRouter,
-  profiles: profilesRouter,
-  locations: locationsRouter,
 });
 
 export type AppRouter = typeof appRouter;
